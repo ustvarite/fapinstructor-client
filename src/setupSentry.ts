578a3dd7ac101034ebcb5e8e8b088e583b2d53d8
thefapinstructor <thefapinstructor@gmail.com>
2021-03-27 16:28:02 -0400

    Don't log sentry errors for google translated content

--- src/setupSentry.ts
+++ src/setupSentry.ts
@@ -5,6 +5,7 @@ if (
   process.env.REACT_APP_SENTRY_RELEASE
 ) {
   Sentry.init({
+    denyUrls: [/translate\.googleusercontent\.com/i],
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_SENTRY_RELEASE,
     environment: process.env.NODE_ENV,
