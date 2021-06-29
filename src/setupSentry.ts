7f137b3039b59f57b976f1525c2954019e333a45
thefapinstructor <thefapinstructor@gmail.com>
2021-06-29 07:51:17 -0400

    Add sentry performance monitoring

--- src/setupSentry.ts
+++ src/setupSentry.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/react";
+import { Integrations as TracingIntegrations } from "@sentry/tracing";
 
 if (
   process.env.NODE_ENV === "production" &&
@@ -6,6 +7,10 @@ if (
 ) {
   Sentry.init({
     denyUrls: [/translate\.googleusercontent\.com/i, /jerkofftocelebs\.com/i],
+    integrations: [new TracingIntegrations.BrowserTracing()],
+    tracesSampleRate: 0.2,
+    // TODO: Convert sample rate to env var if decided this feature is greenlighted.
+    // tracesSampleRate: Number(process.env.REACT_APP_SENTRY_TRACE_SAMPLE_RATE),
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_SENTRY_RELEASE,
     environment: process.env.NODE_ENV,
