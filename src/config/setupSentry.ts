d629673c0f3e211196ed85808897699f95b10bc7
thefapinstructor <thefapinstructor@gmail.com>
2021-12-16 09:49:44 -0500

    Descrease sample rate and only trace errors from site

--- src/config/setupSentry.ts
+++ src/config/setupSentry.ts
@@ -6,11 +6,9 @@ if (
   process.env.REACT_APP_SENTRY_RELEASE
 ) {
   Sentry.init({
-    denyUrls: [/translate\.googleusercontent\.com/i, /jerkofftocelebs\.com/i],
+    allowUrls: [/https?:\/\/(www\.)?fapinstructor\.com/],
     integrations: [new TracingIntegrations.BrowserTracing()],
-    tracesSampleRate: 0.2,
-    // TODO: Convert sample rate to env var if decided this feature is greenlighted.
-    // tracesSampleRate: Number(process.env.REACT_APP_SENTRY_TRACE_SAMPLE_RATE),
+    tracesSampleRate: 0.1,
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_SENTRY_RELEASE,
     environment: process.env.NODE_ENV,
